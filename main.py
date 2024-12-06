b2d7c131bd48d0dae6d90f2d446ec945dcea7057
kvasssr <EvaSilk.ES08@gmail.com>
2024-12-06 14:57:08 +0700

    hi version

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ def my_superfunction():
 
 
 def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+    print(1)
     my_superfunction()
 
 
